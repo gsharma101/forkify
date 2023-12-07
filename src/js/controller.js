d13b7b3475a46e8bdc9105b81a0a24ac58da9440
Gaurav Sharma <sharmag8121@gmail.com>
2023-12-08 00:36:11 +0530

    Rendering the Recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
+import 'core-js/stable';
+import 'regenerator-runtime';
 
 console.log(icons);
 
@@ -17,9 +19,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 const showRecipe = async function () {
   try {
     // 1 Loading Recip
+    renderSpinner(recipeContainer);
     const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
     const data = await res.json();
 
